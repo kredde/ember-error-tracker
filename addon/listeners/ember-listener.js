f30a114b87919dd52318af16c4fd98f4518b9348
Kristian <kr3dde@gmail.com>
2019-04-10 17:09:29 +0200

    Add RSVP error listener to ember listeners

--- addon/listeners/ember-listener.js
+++ addon/listeners/ember-listener.js
@@ -1,6 +1,18 @@
 export default class EmberListener {
 
   listen(consumer) {
+    // consume RSVP errors
+    // eslint-disable-next-line
+    Ember.RSVP.onerror = function (error) {
+      const errorLog = {
+        source: 'ember',
+        timestamp: Date.now(),
+        error
+      }
+      consumer.consumeError(errorLog)
+    };
+
+    // consume ember errors
     // eslint-disable-next-line
     Ember.onerror = (error) => {
       const errorLog = {
